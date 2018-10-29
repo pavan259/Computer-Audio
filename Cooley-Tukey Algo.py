99480415074994b3171570b29196589e5860e54e
pavan259 <44563119+pavan259@users.noreply.github.com>
2018-10-29 14:56:44 -0600

    Update Cooley-Tukey Algo.py

--- Cooley-Tukey Algo.py
+++ Cooley-Tukey Algo.py
@@ -16,25 +16,25 @@ N = 128                            # Usisng the Windowing function to reduce the
 def fft(f):
     F = list()
     for k in range(0, N):
-        window = 1 # np.sin(np.pi * (k+0.5)/N)**2
+        window = 1                 # np.sin(np.pi * (k+0.5)/N)**2
         F.append(np.complex(f[k] * window, 0)) #adding complex values in list
  
     fft_rec(F)
     return F
  
 def fft_rec(F):
-    N = len(F)           #Getting current list capacity for checking the minimum size of list and for diving them in odd and even
+    N = len(F)                      #Getting current list capacity for checking the minimum size of list and for diving them in odd and even
  
-    if N <= 1:           #checking up minimum threshold value required for equation that is greater then 1  
+    if N <= 1:                      #checking up minimum threshold value required for equation that is greater then 1  
         return
  
-    even = np.array(F[0:N:2])   #Sorting the value on base of odd and even values of N
+    even = np.array(F[0:N:2])        #Sorting the value on base of odd and even values of N
     odd = np.array(F[1:N:2])
  
-    fft_rec(even)      #recursive calling method fft_rec for sorting the next value
+    fft_rec(even)                    #recursive calling method fft_rec for sorting the next value
     fft_rec(odd)
  
-    for k in range(0, N//2):              #doing N/2 operations as data is divided in 2 parts odd and even
+    for k in range(0, N//2):         #doing N/2 operations as data is divided in 2 parts odd and even
         t = np.exp(np.complex(0, -2 * np.pi * k / N)) * odd[k] 
         F[k] = even[k] + t
         F[N//2 + k] = even[k] - t
@@ -56,7 +56,7 @@ powers = powers_all[0:N//2]
 frequencies = np.divide(np.multiply(SAMPLE_RATE, np.arange(0, N//2)), N)
 plots[1].plot(frequencies, powers)
  
-plt.show() #will provide plot of result
+plt.show()                               #will provide plot of result
 """
 Spyder Editor
 This is a temporary script file.
